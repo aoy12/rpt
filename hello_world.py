338b8a2f2782ac68348c89ec826cd36a3aa0ccbe
A, Y <ayus011@aucklanduni.ac.nz>
2016-09-18 15:25:11 +1200

    heloooo

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:54:25) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 1+1
+2
+>>> print(hello,world)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    print(hello,world)
+NameError: name 'hello' is not defined
+>>> print(hello,world)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    print(hello,world)
+NameError: name 'hello' is not defined
+>>> print('hello,world')
+hello,world
+>>> 
---
+++ hello_world1.py
@@ -0,0 +1,15 @@
+#  this is y first script
+
+phrase =  "jeasdsad"
+
+print(phrase)   # this line displys phrase
+
+long_string = '''This is a
+string that spans across multiple lines'''
+
+print(long_string)
+
+long_string = """This is a new string
+that spans across two lines"""
+
+print(long_string)
